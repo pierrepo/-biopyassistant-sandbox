0bfe9c55c11d053f7494964d4c9cbc4b95e34913
uvsq22004047 <essmay.touami@ens.uvsq.fr>
2024-07-01 22:37:27 +0200

    Improve the script fo ckeck urls.

--- src/analysis/check_url.py
+++ src/analysis/check_url.py
@@ -1,12 +1,43 @@
+""" Check if a URL is valid and if an anchor is valid.
+
+This script reads a file containing URLs and checks if each URL is valid. If the URL is valid, it also checks if the anchor is valid.
+
+Usage:
+======
+    python src/analysis/check_url.py <file_name>
+
+Arguments:
+==========
+    file_name: str
+        The path to the file containing URLs.
+
+Example:
+========
+    python src/analysis/check_url.py chroma_db_chunks_details.txt
+
+This command will read the file `chroma_db_chunks_details.txt` and check each URL in the file. 
+If the URL is valid, it will also check if the anchor is valid.
+
+"""
+
 import sys
 from urllib.parse import urlparse
 from urllib.request import urlopen
 
+from loguru import logger
 
-def verify_args():
-    if len(sys.argv) != 2:
-        sys.exit("Error! Need a URL as argument.")
-    return sys.argv[1]
+
+def get_url(file_name):
+    logger.info(f"Reading URLs from file: {file_name}")
+    urls = set()
+    with open(file_name, 'r') as f:
+        for line in f:
+            if "Url:" in line:
+                url = line.split("Url:")[1].strip()
+                urls.add(url)
+    logger.success(f"Found {len(urls)} URLs in the file.")
+
+    return urls
 
 
 def is_valid_url(url):
@@ -20,19 +51,27 @@ def is_valid_url(url):
 def is_valid_anchor(url):
     anchor = urlparse(url).fragment
     r = urlopen(url)
-    if anchor in str(r.read()):
-        print("Anchor: OK")
-    else:
-        print("Anchor: ERROR")
+    if anchor not in str(r.read()):
+        logger.info(f"Checking URL: {url}")
+        logger.error("Anchor: ERROR")
        
-        
+
 if __name__ == "__main__":
-    url = verify_args()
-    print(f"Checking URL: {url}")
-    if is_valid_url(url):
-        print("URL: OK")
-    else:
-        print("URL: ERROR")
-    if "#" in url:
-        is_valid_anchor(url)
+    # Get the file name from the command line
+    if len(sys.argv) != 2:
+        sys.exit("Error! Need exactly one argument: the path to the file containing URLs.")
+    
+    file_name = sys.argv[1]
+
+    # Get the URLs from the file and check each URL
+    urls = get_url(file_name)
+    for url in urls:
+        if not is_valid_url(url):
+            logger.info(f"Checking URL: {url}")
+            logger.error("URL: ERROR")
+        else:
+            if "#" in url:
+                is_valid_anchor(url)
+
+    logger.success("Done checking URLs.")
     
